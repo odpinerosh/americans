5132c2e9e5c3e5f6b5e993ea084e474b155b9dc8
odpinerosh <odpinerosh@gmail.com>
2017-08-30 23:09:03 -0500

    Formulario de Contacto

---
+++ js/code.js
@@ -0,0 +1,29 @@
+function validarForm() {
+  "use strict";
+/*  var x = document.getElementById("validationCustom01");
+  var y = document.getElementById("validationCustom02");
+  if (x.value.length==0 || y.value.length==0) {
+    alert("Debes escribir tu nombre y tu apellido.");
+    return false;
+  }
+
+/*  if (x.value==0) {
+    alert("Cero!");
+  }
+  else {
+
+  }*/
+};
+
+/*  window.addEventListener("load", function() {
+    var form = document.getElementById("needs-validation");
+    form.addEventListener("submit", function(event) {
+      if (form.checkValidity() == false) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+      form.classList.add("was-validated");
+    }, false);
+  }, false);
+}());
+*/
